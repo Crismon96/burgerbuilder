aa0fb2638af86a292b78d45cf1768dc22c57c293
crismon96 <christoph.griehl@kodira.de>
2020-01-23 16:39:53 +0100

    added first component

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const layout = (props) => (
+    <>
+        <div></div>
+        <main>
+            {props.children}
+        </main>
+    </>);
+
+export default layout;
